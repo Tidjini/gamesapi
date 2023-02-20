856072b86a931cd84e4af70e81b7943f5cb127b9
tidjini <tidjini.messaoudi@gmail.com>
2023-02-20 21:27:03 +0100

    set Game Model in Games Application

--- games/models.py
+++ games/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+class Game(models.Model):
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=200)
+    release_date = models.DateField()
+    category = models.CharField(max_length=200)
+    played = models.BooleanField()
+
+    class Meta:
+        ordering = 'name', 
