2b30e02e29696de3096332553d633ba3d68ee593
tidjini <tidjini.messaoudi@gmail.com>
2023-02-20 21:21:49 +0100

    set SECRET KEY in .env

--- gameapi/settings.py
+++ gameapi/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
-
+from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-rt4n#mj#t(n0)8n)^n%5-+^#7-r@6u#d3@a49susv2b7_v4zm^"
-
+SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
